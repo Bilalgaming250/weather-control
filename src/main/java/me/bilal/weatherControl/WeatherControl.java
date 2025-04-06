24f8cf20e6ebc8fb2417164303cb1bd2f9be1035
Bilalgaming <76534698+Bilalgaming250@users.noreply.github.com>
2025-04-06 18:03:17 +0530

    Initial Commit

---
+++ src/main/java/me/bilal/weatherControl/WeatherControl.java
@@ -0,0 +1,17 @@
+package me.bilal.weatherControl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WeatherControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
